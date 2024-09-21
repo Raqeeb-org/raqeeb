6b8b3a4c93d0653be5bdc0b6f410332ab6fcf42a
Hala Alnadeem <hala.alnadeem@hotmail.com>
2024-09-21 04:22:17 +0300

    Refactor: import statements in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:raqeeb/screens/commons/login.dart';
-import 'package:raqeeb/screens/commons/reset_password.dart';
-import 'package:raqeeb/screens/commons/verification.dart';
+//import 'package:raqeeb/screens/commons/reset_password.dart';
+//import 'package:raqeeb/screens/commons/verification.dart';
 
 void main() {
   runApp(RaqeebApp());
