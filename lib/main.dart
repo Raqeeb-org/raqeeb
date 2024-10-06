3088952df50ef038db56f80a662b779f3101f2e2
Hala Alnadeem <hala.alnadeem@hotmail.com>
2024-10-06 14:57:30 +0300

    Revert "Merge pull request #10 from Raqeeb-org/basma4"
    This reverts commit e3826d47b75611bdb7b0ddae0e4d5ee232933ed7, reversing
    changes made to 575856bdf9a05d28771903d4615506fe26ccce7b.

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:raqeeb/screens/commons/login.dart';
 //import 'package:raqeeb/screens/commons/reset_password.dart';
 //import 'package:raqeeb/screens/commons/verification.dart';
-//firebase
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:get/get.dart';
 
-// ...
-
-/*void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(RaqeebApp());  // Change this to match your class name
+void main() {
+  runApp(RaqeebApp());
 }
-
-
 
 class RaqeebApp extends StatelessWidget {
   const RaqeebApp({super.key});
@@ -31,58 +19,6 @@ class RaqeebApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: LoginPage(), // Set the LoginPage as the home page
-    );
-  }
-}
-
-class RaqeebApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Firebase Connected!'),
-      ),
-      body: Center(
-        child: Text('Firebase is working!'),
-      ),
-    );
-  }
-}*/
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(RaqeebApp());
-}
-
-class RaqeebApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Ensure that MaterialApp is here
-      title: 'Firebase Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:
-          MyHomePage(), // Your home widget, ensure this is wrapped inside MaterialApp
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // Scaffold should be wrapped inside MaterialApp
-      appBar: AppBar(
-        title: Text('Firebase Connected!'),
-      ),
-      body: Center(
-        child: Text('Firebase is working!'),
-      ),
     );
   }
 }
