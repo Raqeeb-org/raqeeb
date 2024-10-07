c398c132523f182dfc8ff97fddbf35f73bceb296
basma2h <basmalhajji2@gmail.com>
2024-10-07 18:26:21 +0300

    Refactor: Initialize firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:raqeeb/screens/commons/login.dart';
 //import 'package:raqeeb/screens/commons/reset_password.dart';
 //import 'package:raqeeb/screens/commons/verification.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(RaqeebApp());
 }
+
+/*
+void main() {
+  runApp(RaqeebApp());
+}*/
 
 class RaqeebApp extends StatelessWidget {
   const RaqeebApp({super.key});
