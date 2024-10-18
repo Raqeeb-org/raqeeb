d503a9f446a3d338c19026ae4eb0466d97595878
Dinah Aburahmah <202012060@yu.edu.sa>
2024-10-18 19:34:42 +0300

    update sweft

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import cloud_firestore
 import firebase_core
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
