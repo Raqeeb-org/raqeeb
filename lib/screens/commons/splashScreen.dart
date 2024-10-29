48af6352d0d9c323bc39ed0ef0527677a1f70665
Hala Alnadeem <hala.alnadeem@hotmail.com>
2024-10-29 05:23:17 +0300

    Refactor splash screen layout and authentication logic

--- lib/screens/commons/splashScreen.dart
+++ lib/screens/commons/splashScreen.dart
@@ -11,7 +11,6 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    // Check auth state after a delay to simulate a splash screen effect
     Future.delayed(const Duration(seconds: 2), () {
       checkAuthentication();
     });
@@ -20,39 +19,58 @@ class _SplashScreenState extends State<SplashScreen> {
   // Method to check Firebase authentication state
   void checkAuthentication() async {
     FirebaseAuth.instance.authStateChanges().listen((User? user) async {
+      print('User: $user');
       if (user == null) {
         // User is not logged in, navigate to login page
         Navigator.pushReplacementNamed(context, '/login');
       } else {
-        // User is logged in, check role and navigate accordingly
+        // User is logged in, fetch the user's role from Firestore
         String userId = user.uid;
-        DocumentSnapshot userDoc = await FirebaseFirestore.instance
+
+        // Firestore document containing subcollections
+        DocumentReference userDoc = FirebaseFirestore.instance
             .collection('Users')
-            .doc('2J4DFh6Gxi9vNAmip0iA')
-            //.collection()
-            //.doc(userId)
-            .get();
+            .doc('2J4DFh6Gxi9vNAmip0iA');
 
-        if (userDoc.exists) {
-          String role = userDoc['role'];
-
-          if (role == 'admin') {
-            Navigator.pushReplacementNamed(context, '/admin_home');
-          } else if (role == 'driver') {
-            Navigator.pushReplacementNamed(context, '/driver_home');
-          } else if (role == 'parent') {
-            Navigator.pushReplacementNamed(context, '/parent_home');
-          }
+        // Check if user exists in Drivers subcollection
+        DocumentSnapshot driverSnapshot =
+            await userDoc.collection('Drivers').doc(userId).get();
+        if (driverSnapshot.exists) {
+          // User is a driver, navigate to driver's home page
+          Navigator.pushReplacementNamed(context, '/driver_home');
+          return;
         }
+
+        // Check if user exists in Parents subcollection
+        DocumentSnapshot parentSnapshot =
+            await userDoc.collection('Parents').doc(userId).get();
+        if (parentSnapshot.exists) {
+          // User is a parent, navigate to parent's home page
+          Navigator.pushReplacementNamed(context, '/parent_home');
+          return;
+        }
+
+        // Check if user exists in Admins subcollection
+        DocumentSnapshot adminSnapshot =
+            await userDoc.collection('Admins').doc(userId).get();
+        if (adminSnapshot.exists) {
+          // User is an admin, navigate to admin's home page
+          Navigator.pushReplacementNamed(context, '/admin_home');
+          return;
+        }
+
+        // If the user is not found in any of the subcollections, you can sign them out
+        FirebaseAuth.instance.signOut();
+        Navigator.pushReplacementNamed(context, '/login');
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.blueAccent,
-      body: Center(
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[100],
+      body: const Center(
         child: CircularProgressIndicator(
           valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
         ),
