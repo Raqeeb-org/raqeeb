cc2edb1b517cc43bcd4abcae8e2b67baf5d68261
Hala Alnadeem <hala.alnadeem@hotmail.com>
2024-10-31 00:28:47 +0300

    refactor: rename parentHomepage class and update layout for better scrolling experience

--- lib/screens/parent/parentHomepage.dart
+++ lib/screens/parent/parentHomepage.dart
@@ -1,157 +1,156 @@
 import 'package:flutter/material.dart';
-//import './bottom_navigation_bar.dart';
 
-class parentHomepage extends StatelessWidget {
+class ParentHomepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      body: Column(
-        children: <Widget>[
-          // Header Section
-          Stack(
-            children: <Widget>[
-              Image.asset(
-                'assets/images/header.png', // Your background image
-                width: double.infinity,
-                height: 200,
-                fit: BoxFit.cover,
-              ),
-              const Positioned(
-                top: 50,
-                left: 20,
-                child: Text(
-                  '',
-                  style: TextStyle(
-                    color: Colors.yellowAccent,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
-          ),
-
-          // My Children Section
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            // Header Section
+            Stack(
               children: <Widget>[
-                const Text(
-                  'MY CHILDREN',
-                  style: TextStyle(
-                    color: Colors.orange,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+                Image.asset(
+                  'assets/images/header.png', // Your background image
+                  width: double.infinity,
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
+                const Positioned(
+                  top: 50,
+                  left: 20,
+                  child: Text(
+                    '',
+                    style: TextStyle(
+                      color: Colors.yellowAccent,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                const SizedBox(height: 10),
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  color: const Color(0xFFFFE08D),
-                  child: const ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: AssetImage(
-                          'assets/images/Abdulla.png'), // Child's photo
-                      radius: 30,
+              ],
+            ),
+
+            // My Children Section
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text(
+                    'MY CHILDREN',
+                    style: TextStyle(
+                      color: Colors.orange,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
-                    title: Text(
-                      'Abdullah',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                  ),
+                  const SizedBox(height: 10),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    color: const Color(0xFFFFE08D),
+                    child: const ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: AssetImage(
+                            'assets/images/Abdulla.png'), // Child's photo
+                        radius: 30,
+                      ),
+                      title: Text(
+                        'Abdullah',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('ID No: GAGT254H'),
+                          Text('ETA: 7:34'),
+                        ],
                       ),
                     ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('ID No: GAGT254H'),
-                        Text('ETA: 7:34'),
-                      ],
+                  ),
+                ],
+              ),
+            ),
+
+            // Schedules Section
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text(
+                    'Schedules',
+                    style: TextStyle(
+                      color: Colors.orange,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-
-          // Schedules Section
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const Text(
-                  'Schedules',
-                  style: TextStyle(
-                    color: Colors.orange,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  color: const Color(0xFFFFE08D),
-                  child: const Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      children: <Widget>[
-                        // Date
-                        Column(
-                          children: <Widget>[
-                            Text(
-                              'Sun',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.orange,
+                  const SizedBox(height: 10),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    color: const Color(0xFFFFE08D),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Row(
+                        children: <Widget>[
+                          // Date
+                          Column(
+                            children: <Widget>[
+                              Text(
+                                'Sun',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.orange,
+                                ),
                               ),
-                            ),
-                            Text(
-                              '09',
-                              style: TextStyle(
-                                fontSize: 36,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.orange,
+                              Text(
+                                '09',
+                                style: TextStyle(
+                                  fontSize: 36,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.orange,
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(width: 20),
-                        // Schedule Info
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Morning trip: Start (6:15 AM)',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            Text(
-                              'Afternoon trip: Start (2:05 PM)',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ],
-                        ),
-                      ],
+                            ],
+                          ),
+                          SizedBox(width: 20),
+                          // Schedule Info
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'Morning trip: Start (6:15 AM)',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              Text(
+                                'Afternoon trip: Start (2:05 PM)',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-
-          const Spacer(),
-        ],
+            const SizedBox(height: 100),
+          ],
+        ),
       ),
-      //bottomNavigationBar: CustomBottomNavigationBar(currentIndex: 0), // Home is not in the bottom nav but represented as index 0
     );
   }
 }
