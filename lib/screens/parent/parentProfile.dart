58b5e960325b8a48c79d6470b7e4a9a5414a7edc
Dinah Aburahmah <202012060@yu.edu.sa>
2024-11-12 02:39:13 +0300

    Refactor: update parent profile

--- lib/screens/parent/parentProfile.dart
+++ lib/screens/parent/parentProfile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:raqeeb/widgets/logoutCard.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:raqeeb/services/auth_service.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:raqeeb/widgets/logoutCard.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -11,18 +11,53 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  bool _isExpanded = false; // Control the expansion for the profile card
+  bool _isExpanded = false;
+  bool _isPasswordExpanded = false;
+
+  // Text controllers for password fields
+  final TextEditingController _currentPasswordController =
+      TextEditingController();
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final ScrollController _scrollController = ScrollController();
+
+  // Visibility toggles for each password field
+  bool _isCurrentPasswordVisible = false;
+  bool _isNewPasswordVisible = false;
+  bool _isConfirmPasswordVisible = false;
+
+  @override
+  void dispose() {
+    _currentPasswordController.dispose();
+    _newPasswordController.dispose();
+    _confirmPasswordController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollToBottom() {
+    Future.delayed(const Duration(milliseconds: 300)).then((_) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
+        controller: _scrollController,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const SizedBox(height: 60),
-            // Profile Title
             const Text(
               'Profile',
               style: TextStyle(
@@ -33,7 +68,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             ),
             const SizedBox(height: 20),
 
-            // Expandable Profile Card (Name and Image + Extra Details)
+            // Profile Information Card
             Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
@@ -57,9 +92,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   children: [
                     Row(
                       children: [
-                        // Profile Image
                         const CircleAvatar(
-                          radius: 35, // Circular profile image
+                          radius: 35,
                           backgroundColor: Colors.orange,
                           child: Icon(
                             Icons.person,
@@ -67,10 +101,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                             color: Colors.white,
                           ),
                         ),
-                        const SizedBox(
-                            width: 15), // Space between image and info
-
-                        // Expanded Column for Name and Phone
+                        const SizedBox(width: 15),
                         const Expanded(
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
@@ -82,8 +113,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                                   fontWeight: FontWeight.bold,
                                   color: Colors.black,
                                 ),
-                                overflow:
-                                    TextOverflow.ellipsis, // Prevents overflow
+                                overflow: TextOverflow.ellipsis,
                               ),
                               Text(
                                 '+966 567 343 77 81',
@@ -91,34 +121,28 @@ class _ProfileScreenState extends State<ProfileScreen> {
                                   fontSize: 16,
                                   color: Colors.blue,
                                 ),
-                                overflow:
-                                    TextOverflow.ellipsis, // Prevents overflow
+                                overflow: TextOverflow.ellipsis,
                               ),
                             ],
                           ),
                         ),
-
-                        // Arrow Button
                         IconButton(
                           icon: AnimatedRotation(
-                            turns: _isExpanded
-                                ? 0.25
-                                : 0, // Rotate arrow when expanded
+                            turns: _isExpanded ? 0.25 : 0,
                             duration: const Duration(milliseconds: 300),
                             child: const Icon(Icons.arrow_forward_ios,
                                 color: Colors.black54),
                           ),
                           onPressed: () {
                             setState(() {
-                              _isExpanded = !_isExpanded; // Toggle expansion
+                              _isExpanded = !_isExpanded;
                             });
                           },
                         ),
                       ],
                     ),
                     if (_isExpanded) ...[
-                      const SizedBox(
-                          height: 20), // Spacing before extra details
+                      const SizedBox(height: 20),
                       _buildExtraDetail('Email', 'muna@example.com'),
                       _buildExtraDetail('Location', 'Riyadh, Saudi Arabia'),
                     ]
@@ -129,27 +153,206 @@ class _ProfileScreenState extends State<ProfileScreen> {
             const SizedBox(height: 20),
 
             // Change Password Card
-            ProfileOptionCard(
-              title: 'Change Password',
-              icon: Icons.lock_outline,
-              onArrowClick: () {},
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFFCC80),
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: Icon(Icons.lock_outline, color: Colors.orange),
+                      title: const Text(
+                        'Change Password',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(
+                          _isPasswordExpanded
+                              ? Icons.keyboard_arrow_up
+                              : Icons.keyboard_arrow_down,
+                          color: Colors.black54,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isPasswordExpanded = !_isPasswordExpanded;
+                          });
+                          if (_isPasswordExpanded) _scrollToBottom();
+                        },
+                      ),
+                    ),
+                    if (_isPasswordExpanded) ...[
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            // Current Password Field
+                            TextField(
+                              controller: _currentPasswordController,
+                              obscureText: !_isCurrentPasswordVisible,
+                              decoration: InputDecoration(
+                                labelText: 'Current Password',
+                                border: OutlineInputBorder(),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    _isCurrentPasswordVisible
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _isCurrentPasswordVisible =
+                                          !_isCurrentPasswordVisible;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+
+                            // New Password Field
+                            TextField(
+                              controller: _newPasswordController,
+                              obscureText: !_isNewPasswordVisible,
+                              decoration: InputDecoration(
+                                labelText: 'New Password',
+                                border: OutlineInputBorder(),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    _isNewPasswordVisible
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _isNewPasswordVisible =
+                                          !_isNewPasswordVisible;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+
+                            // Confirm Password Field
+                            TextField(
+                              controller: _confirmPasswordController,
+                              obscureText: !_isConfirmPasswordVisible,
+                              decoration: InputDecoration(
+                                labelText: 'Confirm Password',
+                                border: OutlineInputBorder(),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    _isConfirmPasswordVisible
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _isConfirmPasswordVisible =
+                                          !_isConfirmPasswordVisible;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+
+                            // Confirm Button
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.red,
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 32, vertical: 12),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                              onPressed: () async {
+                                String currentPassword =
+                                    _currentPasswordController.text;
+                                String newPassword =
+                                    _newPasswordController.text;
+                                String confirmPassword =
+                                    _confirmPasswordController.text;
+
+                                if (newPassword != confirmPassword) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content:
+                                            Text('New passwords do not match')),
+                                  );
+                                  return;
+                                }
+
+                                User? user = _auth.currentUser;
+                                if (user == null) return;
+
+                                try {
+                                  AuthCredential credential =
+                                      EmailAuthProvider.credential(
+                                    email: user.email!,
+                                    password: currentPassword,
+                                  );
+                                  await user
+                                      .reauthenticateWithCredential(credential);
+
+                                  await user.updatePassword(newPassword);
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text(
+                                            'Password updated successfully')),
+                                  );
+
+                                  _currentPasswordController.clear();
+                                  _newPasswordController.clear();
+                                  _confirmPasswordController.clear();
+
+                                  setState(() {
+                                    _isPasswordExpanded = false;
+                                  });
+                                } catch (e) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                        content: Text(
+                                            'Failed to change password: ${e.toString()}')),
+                                  );
+                                }
+                              },
+                              child: const Text('Confirm',
+                                  style: TextStyle(color: Colors.white)),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ],
+                ),
+              ),
             ),
             const SizedBox(height: 20),
 
             // Logout Card
-
-            // Logout Widget
             ProfileOptionCard(
               title: 'Logout',
               icon: Icons.logout,
               expandable: true,
               message: 'Are you sure you want to logout?',
               onArrowClick: () async {
-                // Calling the sign-out method from your AuthService
                 AuthService authService = AuthService();
                 await authService.signOut();
-
-                // Navigate to the login screen
                 Navigator.pushReplacementNamed(context, '/login');
               },
             ),
@@ -159,7 +362,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
-  // Helper widget to build extra details for the profile card
   Widget _buildExtraDetail(String label, String value) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0),
