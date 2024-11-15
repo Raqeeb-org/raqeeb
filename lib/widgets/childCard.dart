4810436a1d30f76b37c881bcc17e4098817f3252
Hala Alnadeem <hala.alnadeem@hotmail.com>
2024-11-15 23:44:56 +0300

    refactor: simplify ChildCard layout and enhance styling with padding and border radius adjustments

--- lib/widgets/childCard.dart
+++ lib/widgets/childCard.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class ChildCard extends StatelessWidget {
   final String name;
   final String id;
-  // final String eta;
   final String imageUrl;
   final bool isCheckedIn;
   final bool isCallEnabled;
@@ -12,7 +11,6 @@ class ChildCard extends StatelessWidget {
     Key? key,
     required this.name,
     required this.id,
-    // required this.eta,
     required this.imageUrl,
     required this.isCheckedIn,
     required this.isCallEnabled,
@@ -27,41 +25,47 @@ class ChildCard extends StatelessWidget {
           color: isCheckedIn
               ? const Color.fromARGB(255, 252, 196, 113)
               : Colors.grey,
-          borderRadius: BorderRadius.circular(20),
+          borderRadius: BorderRadius.circular(25),
         ),
-        child: ListTile(
-          leading: CircleAvatar(
-            backgroundImage: AssetImage(imageUrl),
-            radius: 30,
-          ),
-          title: Text(
-            name,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              fontSize: 16,
-            ),
-          ),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
             children: [
-              Text(
-                'ID No. $id',
-                style: const TextStyle(
-                  color: Colors.white70,
+              // Custom-sized CircleAvatar
+              CircleAvatar(
+                backgroundImage: AssetImage(imageUrl),
+                radius: 30,
+              ),
+              const SizedBox(width: 16), // Add spacing between avatar and text
+              // Name and ID Column
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      'ID No. $id',
+                      style: const TextStyle(
+                        color: Colors.white70,
+                      ),
+                    ),
+                    Text(
+                      'Status: ${isCheckedIn ? 'Checked In' : 'Not Checked In'}',
+                      style: const TextStyle(
+                        color: Colors.white70,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Text(
-                'Status: ${isCheckedIn ? 'Checked In' : 'Not Checked In'}',
-                style: const TextStyle(
-                  color: Colors.white70,
-                ),
-              ),
-            ],
-          ),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
+              // Trailing Icons
               if (isCallEnabled)
                 IconButton(
                   icon: const Icon(Icons.call, color: Colors.white),
