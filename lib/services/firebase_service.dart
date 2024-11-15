7028210aed394357ecaf4cd9654fd6ba4e295c1e
Hala Alnadeem <hala.alnadeem@hotmail.com>
2024-11-15 20:42:26 +0300

    feat: add FirebaseService for managing Firestore and FirebaseAuth interactions

---
+++ lib/services/firebase_service.dart
@@ -0,0 +1,34 @@
+// firebase_service.dart
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Method to get the current admin's ID
+  String? getCurrentAdminId() {
+    return _auth.currentUser?.uid;
+  }
+
+  // Stream to retrieve buses based on the admin ID
+  Stream<QuerySnapshot> getBusesForAdmin(String adminId) {
+    return _firestore
+        .collection('Buses')
+        .where('adminID', isEqualTo: adminId)
+        .snapshots();
+  }
+
+  // Method to retrieve driver data based on driver reference
+  Future<DocumentSnapshot> getDriverData(DocumentReference driverRef) {
+    return driverRef.get();
+  }
+
+  // Stream to retrieve children assigned to a specific bus
+  Stream<QuerySnapshot> getChildrenForBus(String busId) {
+    return _firestore
+        .collection('Children')
+        .where('bus', isEqualTo: busId)
+        .snapshots();
+  }
+}
