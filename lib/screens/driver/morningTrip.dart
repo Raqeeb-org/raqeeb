e12914b36f7b0efa50b23cf32c4b691162d8b7e5
Hala Alnadeem <hala.alnadeem@hotmail.com>
2024-12-03 02:04:52 +0300

    refactor: streamline user input validation in AddDriverScreen and AddParentScreen

--- lib/screens/driver/morningTrip.dart
+++ lib/screens/driver/morningTrip.dart
@@ -1,206 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:raqeeb/screens/commons/location_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:raqeeb/widgets/childCard.dart';
+import 'package:raqeeb/services/firebase_service.dart';
 import 'package:raqeeb/screens/driver/DriverMapScreen.dart';
-import 'package:url_launcher/url_launcher.dart'; // Import url_launcher for phone functionality
+import 'package:url_launcher/url_launcher.dart';
 
 class MorningTripScreen extends StatelessWidget {
-  final LocationService _locationService = LocationService(); // Initialize LocationService
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseService _firebaseService = FirebaseService();
+  final String driverId; // Driver's ID
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.lightBlue[100],
-      appBar: AppBar(
-        title: Text("Morning Trip"),
-        backgroundColor: Colors.lightBlue[200],
-        elevation: 0,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView(
-              padding: const EdgeInsets.all(8.0),
-              children: [
-                studentCard(
-                  name: "Khaled",
-                  id: "GAG17236H",
-                  eta: "6:00",
-                  avatar: "assets/images/Khaled-2.png",
-                  isOnBoard: false,
-                  phoneNumber: "0598765432", // Khaled's phone number
-                ),
-                studentCard(
-                  name: "Deena",
-                  id: "GAG17236H",
-                  eta: "6:30",
-                  avatar: "assets/images/Deena.png",
-                  isOnBoard: true,
-                  phoneNumber: "0598765433", // Deena's phone number
-                ),
-                studentCard(
-                  name: "Haneen",
-                  id: "GAG17236H",
-                  eta: "6:45",
-                  avatar: "assets/images/Haneen-2.png",
-                  isOnBoard: true,
-                  phoneNumber: "0598765434", // Haneen's phone number
-                ),
-                studentCard(
-                  name: "Azeez",
-                  id: "GAG17236H",
-                  eta: "7:15",
-                  avatar: "assets/images/Azeez-2.png",
-                  isOnBoard: false,
-                  phoneNumber: "0598765435", // Azeez's phone number
-                ),
-                studentCard(
-                  name: "Abdullah",
-                  id: "GAG17236H",
-                  eta: "7:34",
-                  avatar: "assets/images/Abdullah.png",
-                  isOnBoard: true,
-                  isCompleted: true, // Indicates the trip is done
-                  phoneNumber: "0598765436", // Abdullah's phone number
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () async {
-                        List<Map<String, dynamic>> studentLocations = [
-                          {"name": "Khaled", "latitude": 24.774265, "longitude": 46.738586},
-                          {"name": "Deena", "latitude": 24.774965, "longitude": 46.739586},
-                          // Add more students as needed
-                        ];
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DriverMapScreen(studentLocations: studentLocations),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                      ),
-                      child: Text(
-                        "Start Trip",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        showReportDialog(
-                            context); // Call the report dialog here
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                      ),
-                      child: Text(
-                        "Report",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 50), // Adds space after buttons for bar
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+  MorningTripScreen({Key? key, required this.driverId}) : super(key: key);
 
-  Widget studentCard({
-    required String name,
-    required String id,
-    required String eta,
-    required String avatar,
-    required bool isOnBoard,
-    required String phoneNumber, // Add phone number as a required parameter
-    bool isCompleted = false,
-  }) {
-    return Card(
-      color: isCompleted ? Colors.grey[400] : Colors.orange[300],
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: ListTile(
-        contentPadding: const EdgeInsets.all(16.0),
-        leading: CircleAvatar(
-          radius: 20,
-          backgroundImage: AssetImage(avatar), // Load the image from assets
-        ),
-        title: Text(
-          name,
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("ID No. $id"),
-            Text("ETA: $eta", style: TextStyle(color: Colors.blue)),
-          ],
-        ),
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            IconButton(
-              icon: Icon(Icons.call, color: Colors.black),
-              onPressed: () {
-                _callFamily(phoneNumber); // Call the predefined number
-              },
-            ),
-            Icon(
-              isOnBoard ? Icons.check_circle : Icons.radio_button_unchecked,
-              color: isOnBoard ? Colors.green : Colors.black,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  Future<String?> _getBusIdForDriver() async {
+    try {
+      // Construct the correct driver reference
+      DocumentReference driverRef = _firestore
+          .collection('Users')
+          .doc('2J4DFh6Gxi9vNAmip0iA')
+          .collection('Drivers')
+          .doc(driverId);
 
-  // Function to make the phone call using url_launcher
-  void _callFamily(String phoneNumber) async {
-    final Uri phoneUri = Uri(
-      scheme: 'tel',
-      path: phoneNumber, // Using the provided phone number
-    );
-    if (await canLaunchUrl(phoneUri)) {
-      await launchUrl(phoneUri); // Launch the phone dialer
-    } else {
-      throw 'Could not launch $phoneUri'; // Handle error if the phone launch fails
+      // Query the bus document based on the driver's reference
+      QuerySnapshot snapshot = await _firestore
+          .collection('Buses')
+          .where('driverID', isEqualTo: driverRef)
+          .get();
+
+      if (snapshot.docs.isNotEmpty) {
+        final busDoc = snapshot.docs.first;
+        print('Bus document data: ${busDoc.data()}'); // Debugging
+        return busDoc.id; // Return the bus ID
+      } else {
+        print('No bus document found for driverID: $driverId');
+      }
+      return null;
+    } catch (e) {
+      print('Error retrieving bus: $e');
+      return null;
     }
   }
 
-  // Method to show the report dialog with school number and call functionality
+  Future<String?> _getParentPhoneNumber(DocumentReference parentRef) async {
+    try {
+      DocumentSnapshot parentDoc = await parentRef.get();
+      if (parentDoc.exists) {
+        return parentDoc[
+            'phoneNumber']; // Assuming 'phoneNumber' exists in the parent document
+      }
+      return null;
+    } catch (e) {
+      print('Error fetching parent phone number: $e');
+      return null;
+    }
+  }
+
   void showReportDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text("School Contact"),
-          content: Text("School number: 0599945789"),
+          title: const Text("School Contact"),
+          content: const Text("School number: 0599945789"),
           actions: <Widget>[
             TextButton(
-              child: Text("Call School"),
+              child: const Text("Call School"),
               onPressed: () async {
-                final Uri phoneUri = Uri(
-                    scheme: 'tel',
-                    path: '0599945789'); // Create the URI for the phone number
+                final Uri phoneUri = Uri(scheme: 'tel', path: '0599945789');
                 if (await canLaunchUrl(phoneUri)) {
                   await launchUrl(phoneUri); // Launch the phone dialer
                 } else {
@@ -209,7 +75,7 @@ class MorningTripScreen extends StatelessWidget {
               },
             ),
             TextButton(
-              child: Text("Close"),
+              child: const Text("Close"),
               onPressed: () {
                 Navigator.of(context).pop(); // Close the dialog
               },
@@ -219,11 +85,158 @@ class MorningTripScreen extends StatelessWidget {
       },
     );
   }
-}
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MorningTripScreen(),
-  ));
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[100],
+      appBar: AppBar(
+        title: const Text("Morning Trip"),
+        backgroundColor: Colors.lightBlue[200],
+        elevation: 0,
+      ),
+      body: FutureBuilder<String?>(
+        future: _getBusIdForDriver(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          if (!snapshot.hasData || snapshot.data == null) {
+            return const Center(child: Text('No bus found for this driver.'));
+          }
+
+          final String busId = snapshot.data!;
+          return Column(
+            children: [
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: _firebaseService.getChildrenForBus(busId),
+                  builder: (context, childrenSnapshot) {
+                    if (childrenSnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    if (childrenSnapshot.hasError) {
+                      return Center(
+                          child: Text('Error: ${childrenSnapshot.error}'));
+                    }
+                    if (!childrenSnapshot.hasData ||
+                        childrenSnapshot.data!.docs.isEmpty) {
+                      return const Center(
+                          child: Text('No children found for this bus.'));
+                    }
+
+                    final childrenDocs = childrenSnapshot.data!.docs;
+                    return ListView.builder(
+                      itemCount: childrenDocs.length,
+                      itemBuilder: (context, index) {
+                        final child = childrenDocs[index];
+                        final parentRef =
+                            child['parentID']; // Parent reference field
+
+                        return FutureBuilder<String?>(
+                          future: _getParentPhoneNumber(parentRef),
+                          builder: (context, parentSnapshot) {
+                            if (parentSnapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            }
+                            if (parentSnapshot.hasError) {
+                              return const Text(
+                                'Error loading parent phone number',
+                                style: TextStyle(color: Colors.red),
+                              );
+                            }
+
+                            final parentPhoneNumber =
+                                parentSnapshot.data ?? 'No Phone Number';
+
+                            return ChildCard(
+                              name:
+                                  '${child['firstName']} ${child['lastName']}',
+                              id: child['idNum'],
+                              imageUrl: child['gender'] == "Female"
+                                  ? "assets/images/femaleChild.png"
+                                  : "assets/images/maleChild.png",
+                              isCheckedIn: child['isCheckedIn'] ?? false,
+                              parentPhoneNumber: parentPhoneNumber,
+                            );
+                          },
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () async {
+                            List<Map<String, dynamic>> studentLocations = [
+                              {
+                                "name": "Khaled",
+                                "latitude": 24.774265,
+                                "longitude": 46.738586
+                              },
+                              {
+                                "name": "Deena",
+                                "latitude": 24.774965,
+                                "longitude": 46.739586
+                              },
+                              // Add more students as needed
+                            ];
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DriverMapScreen(
+                                    studentLocations: studentLocations),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 15),
+                          ),
+                          child: const Text(
+                            "Start Trip",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            showReportDialog(context);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 15),
+                          ),
+                          child: const Text(
+                            "Report",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                        height: 50), // Adds space after buttons for bar
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
 }
